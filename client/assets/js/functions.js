9d35c48009d907ef0b34ac069e84a65f9cec0c67
sergioromerodev <ser.romero89@gmail.com>
2018-12-16 11:46:17 +0100

    prod client

--- client/assets/js/functions.js
+++ client/assets/js/functions.js
@@ -1,12 +1,12 @@
 $(document).on('click', '.navbar-toggler[aria-expanded="false"]', function(){
 	$('header .container').addClass('fixed-top');
 	$('body').css({'height': '100%', 'overflow': 'hidden'});
-	var h = $('header').heigth();
-	$('#main').css('margin-top': h);
+	var h = $('header .container').height();
+	$('#main').css('margin-top', h);
 });
 
 $(document).on('click', '.navbar-toggler[aria-expanded="true"]', function(){
 	$('header .container').removeClass('fixed-top');
 	$('body').css({'height': 'initial', 'overflow': 'initial'});
-	$('#main').css('margin-top': '0');
+	$('#main').css('margin-top', '0');
 });
