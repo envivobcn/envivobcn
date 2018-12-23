3ca7c25d4cba9e47a5d08b598bcdf25ec72c13e2
sergioromerodev <ser.romero89@gmail.com>
2018-12-23 16:34:28 +0100

    htps fix

--- db.js
+++ db.js
@@ -4,23 +4,16 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3978;
 var http = require('http');
-var https = require('https');
-var fs = require("fs");
-var https = require('https');
-var server = https.createServer(options, app);
-var options = {
-	key: fs.readFileSync('/etc/letsencrypt/live/envivo.barcelona/privkey.pem'), 
-	cert: fs.readFileSync('/etc/letsencrypt/live/envivo.barcelona/fullchain.pem')
-};
+var server = http.createServer(app);
 
 mongoose.connect('mongodb://localhost:27017/envivobcn', (err, res) => {
 	if(err){
 		throw err;
 	}else{
-		console.log("BCNENVIVO DDBB connection running");
+		console.log("BCNENVIVO Prod DDBB connection running");
 
 		server.listen(port, function(){
-			console.log("Server API Rest BCNENVIVO listening");
+			console.log("Server Prod API Rest BCNENVIVO listening");
 		})
 	}
 });
